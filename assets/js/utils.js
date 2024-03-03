3d5babb7f5b37bfb80bbe8ffac8d299332f6e3e2
Sindhu M <sindhu.m@catina.com>
2024-03-04 02:14:50 +0530

    api changes

---
+++ assets/js/utils.js
@@ -0,0 +1,9 @@
+// function to get url parameter
+function getUrlParameter(name) {
+  name = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  var results = regex.exec(location.search);
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
